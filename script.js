86cbd5fcbab350610f358fe12ebdd29f3711c208
MindulaHewadewage <mindufernando@gmail.com>
2022-12-06 09:12:25 +0100

    creando struttura principale del js

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 console.log('JS OK')
 
 // Metto il DOM nel JS
-const ticketPrice = document.getElementById('train-ticket-price')
-console.log('ticketPrice')
+const trainTicketPrice = document.getElementById('train-ticket-price');
+console.log('trainTicketPrice');
 
-// Chiedo al cliente quanti chilometri vuoile fare
-const tripLenght = prompt('Quanti KM vuoi percorrere?')
-console.log('tripLenght')
+// Chiedo al cliente quanti chilometri vuole fare
+const tripLenght = parseInt(prompt('Quanti KM vuoi percorrere?', '20'));
+console.log('tripLenght');
+
+// Chiedo l'eta al cliente
+const userAge = prompt('Quanti Anni hai?', '18');
+console.log('userAge');
+
+// Creo una variabile con il prezzo del biglietto a kilometro
+const kmPrice = parseInt(0.21);
+console.log('kmPrice');
+
+// Calcolo il prezzo del biglietto
+const ticketPrice = tripLenght * kmPrice;
+console.log('ticketPrice');
 
