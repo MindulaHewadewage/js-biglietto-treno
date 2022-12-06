3e3e94cb0ea62f7f7d22c135062469115e919fec
MindulaHewadewage <mindufernando@gmail.com>
2022-12-06 08:57:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('JS OK')
+
+// Metto il DOM nel JS
+const ticketPrice = document.getElementById('train-ticket-price')
+console.log('ticketPrice')
+
+// Chiedo al cliente quanti chilometri vuoile fare
+const tripLenght = prompt('Quanti KM vuoi percorrere?')
+console.log('tripLenght')
+
